37362d15cc9c079d6cc3cd131f4ff751a55d1101
KonstantinIvanchenko <ikv.mstu@gmail.com>
2018-06-24 21:07:09 +0200

    Working model

--- localClientMain.py
+++ localClientMain.py
@@ -8,33 +8,118 @@ from time import sleep
 
 import numpy as np
 
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from threading import Thread, Event
 
 #Filename
 Filename = 'SensorDataOut.csv'
 #number data points
-ndataPoints = 100
+ndataPoints = 3
 #new update
 glUpdateState = False
 #update counter
 glUpdateCounter = 0
-#update counter for "time"
-glTimeUpdateCounter = 0
+#update counter for "index"
+glIndexCounter = 0
 #number of messages
 glNumberOfMessages = 3
-
+#initial values. Will be used as 0 for unsubcribed topics
 newCPULoad = 0
 newCPUTemp = 0
 newVirtMemUsed = 0
 newVirtMemAvail = 0
 
-dataFieldNames = ['CpuTemp','CpuLoad','VirtMemUsed','VirtMemAvail']
+#random thread handlers
+threadDataSender = Thread()
+thread_stop_event_rand = Event()
 
-raw_assocData = {'CpuTemp' : pd.Series(0, index=list(range(ndataPoints)),dtype='float32' ),
-                 'CpuLoad' : pd.Series(0, index=list(range(ndataPoints)),dtype='float32' ),
-                 'VirtMemUsed' : pd.Series(0, index=list(range(ndataPoints)), dtype='int32' ),
-                 'VirtMemAvail' : pd.Series(0, index=list(range(ndataPoints)),dtype='int32' )}
+#flask thread handlers
+threadFlask = Thread()
+thread_stop_event_flask = Event()
 
-pd_frame = pd.DataFrame(raw_assocData, dataFieldNames)
+#register name of app in flask
+app = Flask(__name__)
+socketio = SocketIO(app)
+##################################################################################### - Browser connectivity
+#DataSenderThread
+class DataSenderThread(Thread):
+    def __init__(self):
+        self.delay = 4# s delay to send data to the browser
+        super(DataSenderThread, self).__init__()
+
+    def dataSend(self):
+        print("Sending datasets..")
+        while not thread_stop_event_rand.isSet():
+
+            dataset = pd.read_csv(Filename)
+            total_rows = dataset.shape[0]
+            index = dataset.iloc[total_rows-100:total_rows,1].astype('int32').tolist()
+            values_cputemp = dataset.iloc[total_rows-100:total_rows,2].astype('int32').tolist()
+            values_cpuload = dataset.iloc[total_rows-100:total_rows,3].astype('int32').tolist()
+            values_virtmused = dataset.iloc[total_rows - 100:total_rows, 4].astype('int64').tolist()
+            values_virtmavai = dataset.iloc[total_rows - 100:total_rows, 5].astype('int64').tolist()
+
+            values_virtmused = [int(x/1000000) for x in values_virtmused]#to Mb
+            values_virtmavai = [int(x /1000000) for x in values_virtmavai]#to Mb
+
+            alldata_to_send = [index, values_cpuload, values_cputemp, values_virtmused, values_virtmavai]
+
+            socketio.emit('new_data', alldata_to_send)
+            sleep(self.delay)
+
+    def run(self):
+        self.dataSend()
+#DataSenderThread - end
+
+#flaskThread
+class FlaskThread(Thread):
+    def __init__(self):
+        super(FlaskThread, self).__init__()
+
+    def flaskServer(self):
+        print("Starting flask thread..")
+        if __name__ == "__main__":
+            app.run()
+
+    def run(self):
+        self.flaskServer()
+#flaskThread - end
+
+
+@app.route("/")
+def index():
+    return render_template('/chart.html')
+
+@socketio.on('connect')
+def test_connect():
+    global threadDataSender
+    print('Client connected')
+
+
+    if not threadDataSender.isAlive():
+        print("Starting thread for sending data to browser..")
+        threadDataSender = DataSenderThread()
+        threadDataSender.start()
+
+
+threadFlask = FlaskThread()
+threadFlask.start()
+
+##################################################################################### -end Browser connectivity
+
+##################################################################################### - Sensor connectivity
+dataFieldNames = ['Index','CpuTemp','CpuLoad','VirtMemUsed','VirtMemAvail']
+dataindex = np.arange(ndataPoints)
+
+raw_assocData = {'Index' : np.zeros(shape=(ndataPoints), dtype=int ),
+                 'CpuTemp' : np.zeros(shape=(ndataPoints), dtype=float ),
+                 'CpuLoad' : np.zeros(shape=(ndataPoints), dtype=float ),
+                 'VirtMemUsed' : np.zeros(shape=(ndataPoints), dtype=int ),
+                 'VirtMemAvail' : np.zeros(shape=(ndataPoints), dtype=int )}
+
+
+pd_frame = pd.DataFrame(raw_assocData, columns=dataFieldNames, index=dataindex)
 
 
 rawDataTime = np.zeros(ndataPoints)
@@ -46,35 +131,20 @@ rawDataCpuLoad = np.zeros(ndataPoints)
 def loadDataLastIndex():
     if os.path.isfile(Filename) and os.path.getsize(Filename) > 0:
         #load from file
-        data = pd.read_csv(Filename, names=dataFieldNames, nrows=1)
-        return data.get_value(0, '')
+        data = pd.read_csv(Filename, names=dataFieldNames) #nrows=1)
+        lastindex = data['Index'].iloc[-1]
+        return lastindex
     else:
         #create a new data structure
         return 0#rawdata
 
 
 def writeDatatoCSV(dataframe):
-    if os.path.isfile(Filename) and os.path.getsize(Filename) > 0:
+    #if os.path.isfile(Filename):
         dataframe.to_csv(Filename, mode='a', header=False)
+    #else:
+       # dataframe.to_csv
 
-'''
-tempTrace = plg.Scatter(x=raw_assocData['Timestamp'], y=raw_assocData['CpuTemp'],
-                        xaxis='Timestamp',yaxis='CpuTemp')
-loadTrace = plg.Scatter(x=raw_assocData['Timestamp'], y=raw_assocData['CpuLoad'],
-                        xaxis='Timestamp', yaxis='CpuLoad')
-virtMemAvailTrace = plg.Scatter(x=raw_assocData['Timestamp'], y=raw_assocData['CpuLoad'],
-                        xaxis='Timestamp', yaxis='VirtMemAvail')
-
-data = plg.Data([tempTrace, loadTrace, virtMemAvailTrace])
-
-figure = plg.Figure(data=data)
-figure = pl.tools.make_subplots(rows=3, cols=1)
-figure.append_trace(tempTrace, 1, 1)
-figure.append_trace(loadTrace, 2, 1)
-figure.append_trace(virtMemAvailTrace, 3, 1)
-
-pl.offline.plot(figure,  filename='Sensor data')
-'''
 
 class Subsriptions(Enum):
     CPUTEMP = 0,
@@ -83,7 +153,7 @@ class Subsriptions(Enum):
 
 def on_message(client, userdata, message):
         global glUpdateCounter
-        global glTimeUpdateCounter
+        global glIndexCounter
         global pd_frame
         global newCPULoad
         global newCPULoad
@@ -93,36 +163,47 @@ def on_message(client, userdata, message):
 
         mesDec = str(message.payload.decode("utf-8"))
         print("message received ", mesDec)
-        print(glTimeUpdateCounter)
+        print(glIndexCounter)
         #print("message topic=", message.topic)
         #print("message qos=", message.qos)
         #print("message retain flag=", message.retain)
 
 
         if (message.topic == "cpuload"):
-            newCPULoad = mesDec
+            newCPULoad = float(mesDec)
         if (message.topic == "cputemp"):
-            newCPUTemp = mesDec
+            if (mesDec != "None"):
+                newCPUTemp = float(mesDec[:len(mesDec)-2])
+            else:
+                newCPUTemp = 25
         if (message.topic == "memories"):
-            mes_list = mesDec.split(',')
-            newVirtMemAvail = mes_list[0]
-            newVirtMemUsed = mesDec[1]
+            mes_list = list(mesDec.split(','))
+            newVirtMemAvail = int(mes_list[0][1:])
+            newVirtMemUsed = int(mes_list[1][1:(len(mes_list[1])-1)])
 
+        #update global counter
+        glUpdateCounter += 1
+        #if the glUpdateCounter reach the amount. glNumberOfMessages is equal to the amount of subcriptions
+        if glUpdateCounter == glNumberOfMessages :
 
-        if glUpdateCounter == glNumberOfMessages:
-            glUpdateCounter = 0
-            glTimeUpdateCounter = glTimeUpdateCounter + 1
+            currentRow = glIndexCounter % ndataPoints
 
-            currentRow = glTimeUpdateCounter % ndataPoints
+            #Cputemp is not always properly recognized by temp reader in remote sensor. insert dummy value
+            if newCPUTemp == "None":
+                newCPUTemp = 25
+
+            pd_frame.at[currentRow, 'Index'] = glIndexCounter
             pd_frame.at[currentRow, 'CpuLoad'] = newCPULoad
             pd_frame.at[currentRow, 'CpuTemp'] = newCPUTemp
             pd_frame.at[currentRow, 'VirtMemUsed'] = newVirtMemUsed
-            pd_frame.at[currentRow, 'VirtMemAvailable'] = newVirtMemAvail
+            pd_frame.at[currentRow, 'VirtMemAvail'] = newVirtMemAvail
 
-            if currentRow == 0:
+            glUpdateCounter = 0
+            glIndexCounter += 1
+            if currentRow == ndataPoints - 1:
                 writeDatatoCSV(pd_frame)
 
-        #Write to dataframe here
+
 '''        
         #update for message data
         if (message.topic == "cpuload"):
@@ -145,12 +226,6 @@ def on_message(client, userdata, message):
 '''
 
 
-
-            #plt.subplot(212)
-            #plt.plot(rawDataTime, rawDataCpuLoad, 'r--')
-
-
-
 class SysSensorSubscriber:
     def __init__(self, ipv4, port, servicesToSubscribe):
         self.client = mqtt.Client("SensorListener")
@@ -159,6 +234,7 @@ class SysSensorSubscriber:
         self.client.on_message = on_message
         self.hostAddress = ipv4
         self.port = port
+        self.number_of_subscriptions = 0
 
         #assign True/False to each service accordingly to input values
         for pair in servicesToSubscribe:
@@ -171,31 +247,38 @@ class SysSensorSubscriber:
 
     def connect_to_broker(self):
         self.client.connect(self.hostAddress, keepalive=60)
-        #sensorSub.client.loop_start()
-        #self.client.loop_forever()
-        self.subscribe_to_topics()
+
+    def client_loopforever(self):
         self.client.loop_forever()
-        #time.sleep(10)
-        #sensorSub.client.loop_stop()
+        # sensorSub.client.loop_start()
+        # self.client.loop_forever()
+
+        # time.sleep(10)
+        # sensorSub.client.loop_stop()
 
     def subscribe_to_topics(self):
         if self.bCpuTemp:
             self.client.subscribe("cputemp")
             print ("Subscribing to topic 'cputemp'")
+            self.number_of_subscriptions +=1
         if self.bCpuLoad:
             self.client.subscribe("cpuload")
             print ("Subscribing to topic 'cpuload'")
+            self.number_of_subscriptions += 1
         if self.bMemories:
             self.client.subscribe("memories")
             print ("Subscribing to topic 'memories'")
+            self.number_of_subscriptions += 1
 
-
+        return self.number_of_subscriptions
+##################################################################################### - end Sensor connectivity
 
 #run argument parser
 arg_parser = marg.InputArguments()
 args = arg_parser.parser.parse_args()
 
-data = loadData()
+#update global index
+glIndexCounter = loadDataLastIndex() + 1
 
 sensorSub = SysSensorSubscriber(args.host, args.p,
                                 ((Subsriptions.CPUTEMP, args.ct),
@@ -203,4 +286,12 @@ sensorSub = SysSensorSubscriber(args.host, args.p,
                                 (Subsriptions.MEMORIES, args.memo)) )
 
 sensorSub.connect_to_broker()
+
+#update global number of messages to wait for in each iterations
+glNumberOfMessages = sensorSub.subscribe_to_topics()
+
+#MQTT subscribe requires run in a loop
+sensorSub.client_loopforever()
+
+
 
---
+++ static/js/application.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+    //connect to the socket server.
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+    var numbers_received = [];
+
+    //receive details from server
+    socket.on('new_data', function(msg) {
+        console.log("Received indices " + msg[0]);
+        console.log("Received load " + msg[1]);
+        console.log("Received temp " + msg[2]);
+        console.log("Received memused " + msg[3]);
+        console.log("Received memavai " + msg[4]);
+        //maintain a list of ten numbers
+
+        var ctx_load = document.getElementById("myChart_load");
+        var ctx_memused = document.getElementById("myChart_virtMemUsed");
+
+        var chart_Data = new Chart(ctx_load, {
+          type: 'line',
+          data: {
+              labels: msg[0],
+              datasets: [{
+                  label: 'Cpu Load, %',
+                  data: msg[1],
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:true
+                      }
+                  }]
+              },
+              animation: false
+          }
+      });
+
+        var chart_Data = new Chart(ctx_memused, {
+          type: 'line',
+          data: {
+              labels: msg[0],
+              datasets: [{
+                  label: 'Virtual Memory Used, Mb',
+                  data: msg[3],
+                  backgroundColor: [
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:false
+                      }
+                  }]
+              },
+              animation: false
+          }
+      });     
+  
+    })
+});
